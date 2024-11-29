2ed4b8e60a06f8e8c7e5ce5759b1f2b387944824
Sebastian Hilger <hilger@kraenk.de>
2024-11-29 09:21:17 +0100

    Update app.blade.php

--- resources/views/components/layouts/app.blade.php
+++ resources/views/components/layouts/app.blade.php
@@ -10,6 +10,8 @@
 
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
+    <meta name="csrf_token" value="{{ csrf_token() }}"/>
+
     <title>{{ $title }}</title>
 
     @vite(['resources/css/app.css', 'resources/js/app.js'])
